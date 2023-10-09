aea38224b3a9a211ffb885b16f1b80aede00ebbe
ub9fbi <zolotoianatolji@yandex.com>
2023-10-09 23:15:50 +0500

    1

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -4,11 +4,15 @@ public class ComplexNumber {
     public double userNumb; // число от пользователя
     public double rezultMulty;  // умножение комплексногго числа на число от пользователя
 
+    public ComplexNumber() {
+
+    }
+
 
     /* метод ofMultiComplexOfNum
     * *считает произведение мнимого числа на чило от пользователя */
     public void ofMultiComplexOfNum() {
-        rezultMulty = realA1 * userNumb + mnimoeB1 * userNumb;
+        double rezultMulty = realA1 * userNumb + mnimoeB1 * userNumb;
     }
     public void argumentCompleksNumb () {
         double argumentInRadians = (Math.atan2(realA1,mnimoeB1) * Math.PI)/180;
