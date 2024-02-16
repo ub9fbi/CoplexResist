763e82998b74e7962a72f1ae70261f8da39b2a54
ub9fbi <zolotoianatolji@yandex.com>
2024-02-16 16:15:13 +0500

    1

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,28 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static ArrayList<Figur> figurList = new ArrayList<Figur>();
     public static void main(String[] args)
     {
         Circl circl = new Circl();
         Rectangle rectangle = new Rectangle();
 
         int k = 0;
+        System.out.println("Выберите фигуру; 1 - круг, 2 - прямоугольник");
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Выберите фигуру; 1 - круг, 2 - прямоугольник");
-        if (k == 1) {
-            circl.inputNumbers();
-            System.out.println("Площадь круга");
-        } else if (k == 2) {
-            rectangle.inputNumbers();
-            System.out.println("Площадь прямоугольника");
+        switch (k) {
+            case (1):
         }
-
-
     }
 }
+
+
+
+
+        if (k == 1) {
+        circl.inputNumbers();
+        System.out.println("Площадь круга");
+        } else if (k == 2) {
+        rectangle.inputNumbers();
+        System.out.println("Площадь прямоугольника");
